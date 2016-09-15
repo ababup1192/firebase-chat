471aeba4507c145bafdc73f10bf811fe24eb63b7
ababup1192 <ababup1192@gmail.com>
2016-09-15 14:39:11 +0900

    Apply style login page

--- src/script/components/login.tsx
+++ src/script/components/login.tsx
@@ -29,11 +29,11 @@ export default class Login extends React.Component<LoginProps, LoginState> {
 
     private handleClickLogin() {
         if (this.state.uid !== "") {
-                this.props.loginAction.login({
-                    uid: this.state.uid,
-                    photoURL: this.state.photoURL,
-                    displayName: this.state.displayName
-                });
+            this.props.loginAction.login({
+                uid: this.state.uid,
+                photoURL: this.state.photoURL,
+                displayName: this.state.displayName
+            });
         } else {
             alert("twitterボタンを押して、認証してからクリックしてください。");
         }
@@ -66,22 +66,27 @@ export default class Login extends React.Component<LoginProps, LoginState> {
     }
 
     public render() {
-        return <form className="loginForm">
-            <input type="button" value="twitter"
-                onClick={ this.handleTwitterAuth.bind(this) }
-                />
-            <img src={ this.state.photoURL } />
-            <div className="InputAddOn">
-                <span className="InputAddOn-item">Name</span>
-                <input
-                    className="InputAddOn-field"
-                    value={ this.state.displayName }
-                    onChange={ this.handleName.bind(this) }
+        return <div className="login-container">
+            <h1>Afao</h1>
+            <div className="login-form">
+                <h2>Log in to your account</h2>
+                <input id="twitter-button" type="button" value="Twitter"
+                    onClick={ this.handleTwitterAuth.bind(this) }
+                    />
+                <img src={ this.state.photoURL } />
+                <div className="InputAddOn">
+                    <i className="fa fa-user InputAddOn-item" aria-hidden="true"></i>
+                    <input
+                        className="InputAddOn-field"
+                        value={ this.state.displayName }
+                        placeholder="Name"
+                        onChange={ this.handleName.bind(this) }
+                        />
+                </div>
+                <input id="login-button" type="button" value="Log in"
+                    onClick={ this.handleClickLogin.bind(this) }
                     />
             </div>
-            <input type="button" value="Log in"
-                onClick={ this.handleClickLogin.bind(this) }
-                />
-        </form>;
+        </div>;
     }
 }
