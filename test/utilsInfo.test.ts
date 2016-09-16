09a9a8516ba61136c0f278f8015fe7047607f64a
ababup1192 <ababup1192@gmail.com>
2016-09-17 01:16:32 +0900

    Create test directory

---
+++ test/utilsInfo.test.ts
@@ -0,0 +1,10 @@
+import {List, Map} from "immutable";
+import {UserInfoUtil} from "../src/script/utils/userInfo";
+
+describe("UserInfoUtil", () => {
+    describe("toFirebaseUserInfo", () => {
+        it("should return double list", () => {
+            chai.assert.isTrue(true);
+        });
+    });
+});
